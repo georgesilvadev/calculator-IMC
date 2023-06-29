0ad6ab1673911b90b96c59dfae6b54c3d50e5090
georgesilvadev <gab.ge.silva74@gmail.com>
2023-06-29 14:54:56 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { notANumber, IMC } from "./utils.js"
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+  if(weightOrHeightIsNotANumber){
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+
+  const result = IMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result){
+  const message = `Seu IMC é ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
